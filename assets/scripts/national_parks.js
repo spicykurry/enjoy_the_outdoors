425aa2a9c1c2fe030c1642a5eb5142c29a663dcc
Kara Imhof <kara.imhof@libertymutual.com>
2022-06-29 09:11:01 -0700

    refactored solution to selects and stored the value selected in 2nd dropdown

--- assets/scripts/national_parks.js
+++ assets/scripts/national_parks.js
@@ -2,32 +2,67 @@
 let filterParkLocation = document.querySelector("#filterByParkLocation")
 let filterParkType = document.querySelector("#filterByParkType")
 let filterViewAll = document.querySelector("#viewAllParks")
-let stateList = document.querySelector("#stateListDropdown")
-let typeList = document.querySelector("#parkTypeListDropdown")
 let theLocationsUL = document.querySelector("#locations")
 let types = document.querySelector("#types")
 
-// This is listening for the user to select "filter by location"
-// and populates the state/location dropdown list
-filterParkLocation.addEventListener("click", function (event) {
+
+//get the dropdown list into the JS file for us to work with
+let searchTypeDDL = document.querySelector("#filterOptions");
+
+//get the locations dropdown so we can work with it
+let locationsDDL = document.querySelector("#stateListDropdown")
+
+//get the types dropdown so we can work with it
+let typesDDL = document.querySelector("#parkTypesListDropdown")
+
+
+//hide all the dropdowns and then check which is supposed to show below
+searchTypeDDL.addEventListener("change", function (event) {
+    locationsDDL.classList.add("d-none");
+    typesDDL.classList.add("d-none");
+
+    if (event.target.value === "location") {
+        generateLocationsDDLOptions()
+        locationsDDL.classList.remove("d-none")
+        typesDDL.classList.add("d-none")
+    }
+
+    if (event.target.value === "type") {
+        generateTypesDDLOptions()
+        typesDDL.classList.remove("d-none")
+        locationsDDL.classList.add("d-none")
+    }
+})
+
+// This is a function that populates state dropdown when user selects to search by location
+
+function generateLocationsDDLOptions() {
+    locationsDDL.innerHTML = `<option value="">Choose A Location</option>`
     locationsArray.forEach((location) => {
-        theLocationsUL.innerHTML += `<li>${location}</li>`
+        locationsDDL.innerHTML += `<option value="${location}">${location}</option>`
     });
-    
-    stateList.classList.remove("d-none")
-})
+}
 
-// This is listening for the user to select "filter by type"
-// and populates the type dropdown list
-filterParkType.addEventListener("click", function () {
-    parkTypesArray.forEach((type)=>{
-        types.innerHTML += `<li>${type}</li>`
+// This is a function that populates type dropdown when a user selects to search by park type
+
+function generateTypesDDLOptions() {
+    typesDDL.innerHTML = `<option value="">Choose A Park Type</option>`
+    parkTypesArray.forEach((type) => {
+        typesDDL.innerHTML += `<option value="${type}">${type}</option>`
     })
-    typeList.classList.remove("d-none")
+}
 
+locationsDDL.addEventListener("change", function (event) {
+
+    let locationSelection = nationalParksArray.filter(nationalPark => nationalPark.State === event.target.value);
+    console.log(locationSelection)
 })
 
-filterViewAll.addEventListener("click", function () {
+typesDDL.addEventListener("change", function (event){
 
-
+    let typeSelection = parkTypesArray.filter(parkType => parkType === event.target.value);
+    console.log(typeSelection)
 })
+
+
+
